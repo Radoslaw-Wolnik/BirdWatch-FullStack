fb08f122eb568738550e92f3492945baf22f2f2b
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-10-08 14:40:48 +0200

    public upload folder

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental: {
-      appDir: true,
-    },
-  }
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['localhost'], // Add your domain here
+  },
+}
   
-  module.exports = nextConfig
+module.exports = nextConfig
